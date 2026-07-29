43c8fad9e3ab24c1b03572968dbfb4606cbd2b4c
User <user@example.com>
2026-07-30 03:35:11 +0900

    v1.5.37: 외나무다리 배경 상단 25% 연하게

--- js/maps.js
+++ js/maps.js
@@ -24,7 +24,7 @@ const TERRAINS = {
     },
     log_bridge: {
         name: "외나무다리",
-        bg: ["#d4936a", "#a899c0", "#6d91bb"],
+        bg: ["#d4936a", "#a899c0", "#92adcc"],
         color: "#652810", outColor: "#3d1708",
         deathZoneY: -16,
         initZoomSpanMin: 42,  // 초기 배율: Y분산 없어 과도하게 줌인되는 현상 방지 (다른 맵 수준으로 맞춤)
