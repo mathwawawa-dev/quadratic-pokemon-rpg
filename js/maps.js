7c58f9cf8c26fc4c344306f0bf6c40f12f2ae6f2
User <user@example.com>
2026-07-30 02:44:04 +0900

    v1.5.17: 외나무다리 배경 검정 20% 추가 혼합

--- js/maps.js
+++ js/maps.js
@@ -24,7 +24,7 @@ const TERRAINS = {
     },
     log_bridge: {
         name: "외나무다리",
-        bg: ["#6f0d28", "#924007", "#150303"],
+        bg: ["#590a20", "#753306", "#110202"],
         color: "#652810", outColor: "#3d1708",
         deathZoneY: -16,
         initZoomSpanMin: 42,  // 초기 배율: Y분산 없어 과도하게 줌인되는 현상 방지 (다른 맵 수준으로 맞춤)
