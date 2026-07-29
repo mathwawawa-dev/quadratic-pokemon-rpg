9a9ba3b0edc5f6789004f61cd33a8d191afd4515
User <user@example.com>
2026-07-30 02:59:19 +0900

    v1.5.22: 외나무다리 배경 흰색 10% 추가

--- js/maps.js
+++ js/maps.js
@@ -24,7 +24,7 @@ const TERRAINS = {
     },
     log_bridge: {
         name: "외나무다리",
-        bg: ["#923b4d", "#a55c38", "#4d3535"],
+        bg: ["#9d4f5f", "#ae6c4c", "#5f4949"],
         color: "#652810", outColor: "#3d1708",
         deathZoneY: -16,
         initZoomSpanMin: 42,  // 초기 배율: Y분산 없어 과도하게 줌인되는 현상 방지 (다른 맵 수준으로 맞춤)
